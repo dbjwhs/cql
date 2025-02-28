decaf36fc3bfdd1e2944e15e7f1d25b516b00ee7
Dennis Jones <dbjlmc@gmail.com>
2025-02-27 18:05:50 -0800

    this file is not needed anymore

--- odds-and-ends/cql/src/cql/project_utils.cpp
+++
@@ -1,8 +0,0 @@
-// MIT License
-// Copyright (c) 2025 dbjwhs
-
-#include "../../../headers/project_utils.hpp"
-
-// define static members for the logger
-std::weak_ptr<Logger> Logger::m_instance;
-std::mutex Logger::m_instance_mutex;
