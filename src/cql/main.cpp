6e34e656f715a061ce0fc4ad4c09438eca587a3e
Dennis Jones <dbjlmc@gmail.com>
2025-05-04 01:40:21 -0700

    Fix bug where I was mixing up where we include the Logger from, we should use the local logger which we do now

--- src/cql/main.cpp
+++ src/cql/main.cpp
@@ -9,7 +9,7 @@
 
 // note the file exists in the cpp-snippets repo, you will need to check this out and have it and cql share the
 // same root directory
-#include "../../cpp-snippets/headers/project_utils.hpp"
+#include "../../include/cql/project_utils.hpp"
 
 /**
  * @brief Print the help message with usage information
